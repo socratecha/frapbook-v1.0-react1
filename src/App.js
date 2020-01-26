0fb3cb201d3c9812a6a8f4c8fa4c784a8089d4cb
Erik Ferragut <erikferragut@gmail.com>
2020-01-25 21:26:40 -0500

    Nested components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
 
+class Thing1 extends Component {
+    render() {
+        return (
+            <div className="Thing1">
+              <h2>Stuff here</h2>
+            </div>
+        );
+    }
+}
+
+class Thing2 extends Component {
+    render() {
+        return (
+            <div className="Thing2">
+              <p><strong>More stuff here</strong></p>
+            </div>
+        );
+    }
+}
+
 class App extends Component {
     render() {
 	return (
 	    <div className="App">
 	      <p>FRAP applications are fun and powerful</p>
+              <Thing1 />
+              <Thing2 />
+              <Thing2 />
 	    </div>
 	);
     }
