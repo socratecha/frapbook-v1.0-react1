27ef97e65d57cdce9f7687b105b21fd0c9dd91cb
Erik Ferragut <erikferragut@gmail.com>
2020-01-25 21:38:18 -0500

    Three stories and a grouping

--- src/stories/collatz.stories.js
+++ src/stories/collatz.stories.js
@@ -9,3 +9,10 @@ export default {
 export const for8 = () => <Collatz number={8} />;
 for8.story = { name: 'For 8' };
 
+export const for101 = () => <Collatz number={101} />;
+for101.story = { name: 'For 101' };
+
+export const seeMultiple = () => <div>
+                                   { [ 17, 22, 57 ].map(x => <Collatz number={x} />) }
+                                 </div>;
+seeMultiple.story = { name: 'See multiple' };
